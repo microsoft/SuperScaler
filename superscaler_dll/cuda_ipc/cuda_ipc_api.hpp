0e21c7874978bdce872dc6b985077f76856000bb
Ze Gan <zegan@microsoft.com>
2020-04-27 17:09:06 +0800

    Remove useless code
    Signed-off-by: Ze Gan <zegan@microsoft.com>

--- superscaler_dll/cuda_ipc/cuda_ipc_api.hpp
+++ superscaler_dll/cuda_ipc/cuda_ipc_api.hpp
@@ -11,18 +11,3 @@ public:
     size_t read(void * data, size_t size);
 };
 
-
-class Board {
-public:
-    void set(void * buffer, size_t size);
-    size_t upload(const void * data, size_t size);
-};
-
-class CommunicationManager {
-public:
-    void set(void * buffer, size_t size, int rankid);
-
-    size_t upload(const void * data, size_t size, int rankid);
-    size_t write(const void * data, size_t size, int rankid);
-    size_t read(void * data, size_t size, int rankid);
-};
