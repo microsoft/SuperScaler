2a75bf4172d77e82aa8eadc50b4d24a82d25e3c5
Wenhao Shi <v-wenhsh@microsoft.com>
2020-05-21 03:54:52 +0000

    Merged PR 780: Executor: Add Test - Add testing for cuda_ipc
    * Add unit test for single process and multiple processes
    * Add utility to create a child process for test, as clean environment is needed for cuds's test.

---
+++ superscaler_dll/executor/test/utils/utils.cpp
@@ -0,0 +1,14 @@
+#include <thread>
+#include <sstream>
+#include <unistd.h>
+
+#include "utils.hpp"
+
+std::string get_thread_unique_name(const std::string& prefix)
+{
+    std::ostringstream string_builder;
+    string_builder << prefix << '_';
+    string_builder << getpid() << '_';
+    string_builder << std::this_thread::get_id();
+    return string_builder.str();
+}
---
+++ superscaler_dll/executor/test/utils/utils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+/**
+* \brief Get thread unique name like: prefix_pid_tid
+* 
+* @param prefix 
+* @return std::string 
+**/
+std::string get_thread_unique_name(const std::string &prefix);
