398dc960f5de6d60de7edf4c1d5c183cd13f1b2b
Cheng Luo <v-cluo@microsoft.com>
2020-05-30 07:12:25 +0000

    Merged PR 798: Plan Generator: Revise class - introduce most generate_plan tasks within plan class
    Overview
    PR for plan. We rearrange the plan class, and create interface to operate the node with plan by _add_node/_remove_node/_get_node_index/
    
    Main Change in this PR:
    1, _add_node function add new node into plan
    2, _remove_node function removes node from plan
    3, _get_plan function get our plan for child class
    4, _get_node_index get the index of specific node

--- plan_gen/plan/plan.py
+++ plan_gen/plan/plan.py
@@ -1,22 +1,69 @@
+import copy
+
+
 class Plan(object):
+    """ An class that generate optimized plan from nodelist.
+        target for nodes whose op equal to plan_type
+    """
 
-    def __init__(self, plan_type="Default", plan_name="Default_Plan"):
-        # The type of plan
-        self.plan_type = plan_type
-        # The name of plan
-        self.plan_name = plan_name
-
-    def generate_plan(self, plan):
-        ''' An empty function, will be overridden by inherited classes
-        '''
-        pass
+    def __init__(self,
+                 plan_name='Default_Plan',
+                 plan_type='Default'):
+        # The name of plan class
+        self.__plan_name = plan_name
+        # The type of plan class
+        self.__plan_type = plan_type
+        # The plan used in this class
+        self.__plan = []
 
     def get_plan_type(self):
-        ''' get the plan_type attr
+        ''' Get the plan_type attr
         '''
-        return self.plan_type
+        return self.__plan_type
 
     def get_plan_name(self):
-        ''' get the plan_name attr
+        ''' Get the plan_name attr
         '''
-        return self.plan_name
+        return self.__plan_name
+
+    def reset_plan(self, node_list):
+        ''' Set plan vary from a node_list
+        '''
+        if node_list is None:
+            self.__plan = []
+        else:
+            self.__plan = copy.deepcopy(node_list)
+
+    def generate_plan(self):
+        ''' Main function, will be overloadding by child class
+        '''
+        return self._get_plan()
+
+    def _add_node(self, node, index=None):
+        ''' add new node into plan
+        '''
+        if isinstance(index, int) and index >= 0 and index < len(self.__plan):
+            self.__plan.insert(index, node)
+        else:
+            self.__plan.append(node)
+
+    def _remove_node(self, node):
+        ''' remove node from plan
+        '''
+        if node in self.__plan:
+            self.__plan.remove(node)
+        else:
+            return
+
+    def _get_plan(self):
+        ''' get plan
+        '''
+        return self.__plan
+
+    def _get_node_index(self, node):
+        ''' remove node from plan
+        '''
+        if node in self.__plan:
+            return self.__plan.index(node)
+        else:
+            return None
--- plan_gen/tests/test_plan.py
+++ plan_gen/tests/test_plan.py
@@ -10,3 +10,45 @@ def test_plan():
     assert(Default_plan.get_plan_type() == 'Default')
     # Test generate_plan() function
     assert(hasattr(Default_plan, 'generate_plan'))
+
+    nodes = [
+        {
+         'device': 'device_0',
+         'name': 'test',
+         'op': 'Allreduce',
+         'output_shapes': [[1, 100]],
+         'tensor_name': 'test',
+         'tensor_type': 1,
+         'input': []
+        },
+        {
+         'device': 'device_1',
+         'name': 'test',
+         'op': 'Allreduce',
+         'output_shapes': [[1, 100]],
+         'tensor_name': 'test',
+         'tensor_type': 1,
+         'input': []
+        }
+        ]
+
+    Default_plan.reset_plan(nodes)
+    # Test generate_plan() function
+    assert(Default_plan.generate_plan() == nodes)
+
+    node = {
+            'device': 'device_2',
+            'name': 'test',
+            'op': 'Allreduce',
+            'output_shapes': [[1, 100]],
+            'tensor_name': 'test',
+            'tensor_type': 1,
+            'input': []
+           }
+    # Test _add_node() function
+    Default_plan._add_node(node)
+    assert(Default_plan._get_node_index(node) == 2)
+
+    # Test _add_node() function
+    Default_plan._remove_node(node)
+    assert(Default_plan._get_node_index(node) is None)
