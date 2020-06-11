b8376f9ff6aea5433ae52afdefa7c53297fdce63
Cheng Luo <v-cluo@microsoft.com>
2020-06-11 03:19:12 +0000

    Merged PR 814: Plan Generator: Add Feature - Add plan_manager and related test
    **Overview**
    This is the first PR of plan_manager.
    
    Current Goal: plan_manager will control other plan-related classes including plan_pool and plan_mapper. It should the core controller of our plan generator
    
    **Main Changes**
    Add plan_manager to control other plan-related classes
    Add related UT

---
+++ plan_gen/plan/plan_manager.py
@@ -0,0 +1,46 @@
+from plan.plan_mapper import PlanMapper
+from plan.plan_pool import PlanPool
+
+
+class PlanManager(object):
+    """ An manager class to control other plan related class including
+        plan_pool and plan_mapper
+    """
+    def __init__(self,
+                 plan_pool,
+                 plan_mapper):
+
+        if not isinstance(plan_pool, PlanPool):
+            raise ValueError("Input plan_pool must be PlanPool instance")
+        if not isinstance(plan_mapper, PlanMapper):
+            raise ValueError("Input plan_mapper must be PlanMapper instance")
+
+        self.__plan_pool = plan_pool
+        self.__plan_mapper = plan_mapper
+
+    def get_execution_plan(self, node_list, plan_type, plan_name):
+        ''' get the plan after generation and mapping
+        Args:
+            node_list: list
+            plan_type: string, e.g. Default
+            plan_name: string, e.g. Default_Plan
+        '''
+        plan = self.__plan_pool.get_plan(plan_type=plan_type,
+                                         plan_name=plan_name)
+        # if the plan_type or plan_name is not correct, we get None
+        # if the node_list is not a list, we can only get a empty list result
+        # so in these cases, we return None as warning
+        if plan is None or not isinstance(node_list, list):
+            return None
+        plan.reset_plan(node_list)
+        output_plan = plan.generate_plan()
+        # None output of generate_plan function means something goes wrong
+        if output_plan is None:
+            return None
+
+        mapped_plan = self.__plan_mapper.map(output_plan)
+        # None output of map() function means something goes wrong
+        if mapped_plan is None:
+            return None
+
+        return mapped_plan
