f1918ffb22d1e1dc123bfbcdc331cc958ce0a85a
Jiahao Wen <v-jiahwe@microsoft.com>
2020-06-16 11:23:13 +0000

    Merged PR 821: AISimulator: Add Feature - Add class Flow and related UT
    ## Overview
    This is the 1st PR of `NetworkSimulator`.
    Final Goal:
    1. Reconstruct network simulation. Given a specific `link_spec` and `routing_info`, the `NetworkSimulator` will simulate and output an estimated execution result of each `Node`
    2. Delete previous code `FairsharingDevice`, `NetworkSwitch`, and related code in `Simulator`. This will be done after the `NetworkSimulator` PR is fully merged.
    
    ## Main Change in Current PR
    1. Add class `Flow` to represent a `Send` node
    2. Add unit test of class `Flow`
    
    ## Note
    This class `Flow` is similar to the previous one. (see [simulator/fairsharing_device.py](https://msrasrg.visualstudio.com/SuperScaler/_git/SuperScaler?path=%2Fai_simulator%2Fsimulator%2Ffairsharing_device.py&version=GBdev&_a=contents))

---
+++ ai_simulator/simulator/network_simulator/flow.py
@@ -0,0 +1,67 @@
+from simulator.utility import data_size_to_bit
+
+
+class Flow():
+
+    def __init__(self, node, time_now):
+        '''Initialize a Flow.
+
+        Args:
+            node: class Node, a Send node
+            time_now: float, current simulation time
+        '''
+        self.node = node
+        self.__total_data_len = 0
+        tensors = node.get_tensors()
+        # Calculate total bytes of tensors
+        for tensor in tensors:
+            self.__total_data_len += tensor.get_bytes_size()
+        # Store current status
+        self.__remain_len = \
+            data_size_to_bit(str(self.__total_data_len) + 'B')  # Stored in bit
+        self.__estimated_finish_time = float('inf')
+        self.__available_bandwidth = 0
+        self.__last_start_time = time_now
+
+    def get_estimated_finish_time(self):
+        '''Return the estimated finish time of current flow'''
+        return self.__estimated_finish_time
+
+    def get_available_bandwidth(self):
+        return self.__available_bandwidth
+
+    def set_available_bandwidth(self, available_bandwidth, time_now):
+        '''Set self.__current_available_bandwidth, then change
+        estimated_finish_time and other flow status
+
+        Args:
+            current_available_bandwidth: a float representing tansfer rate(bps)
+            time_now: current time of simulation
+        Calculate total executed bytes during [last_start_time, time_now],
+        then update __remain_len and calculate new __estimated_finish_time
+        '''
+        if available_bandwidth < 0:
+            raise ValueError("Bandwidth should not be a negative number!")
+        if time_now < self.__last_start_time:
+            raise ValueError(
+                "time_now should not be less than previous start time!")
+
+        executed_bits = (time_now - self.__last_start_time) \
+            * self.__available_bandwidth
+        self.__remain_len -= executed_bits
+        if available_bandwidth == 0:
+            self.__estimated_finish_time = float('inf')
+        else:
+            self.__estimated_finish_time = time_now \
+                + self.__remain_len / available_bandwidth
+        self.__last_start_time = time_now
+        self.__available_bandwidth = available_bandwidth
+
+    def __lt__(self, other):
+        '''Flow is ordered by estimated_finish_time'''
+        if self.__estimated_finish_time < other.__estimated_finish_time:
+            return True
+        elif self.__estimated_finish_time == other.__estimated_finish_time:
+            return self.node.get_index() < other.node.get_index()
+        else:
+            return False
---
+++ ai_simulator/tests/test_network_simulator/test_flow.py
@@ -0,0 +1,51 @@
+import pytest
+
+from simulator.tensor import Tensor
+from simulator.fifo_device import FIFODevice
+from simulator.node import Node, NodeMetadata
+from simulator.network_simulator.flow import Flow
+
+
+def test_flow():
+    tensor_unit = Tensor('int32', 1)
+    tensor_size = 5
+    # Init flow with same time_now and capacity
+    flow_0 = create_test_flow(0, 'send', '/switch/switch0/', tensor_size, 10)
+    flow_0.set_available_bandwidth(tensor_unit.get_bytes_size() * 8, 10)
+    flow_1 = create_test_flow(0, 'send', '/switch/switch0/',
+                              tensor_size + 1, 10)
+    flow_1.set_available_bandwidth(tensor_unit.get_bytes_size() * 8, 10)
+    # Test Flow comparision operation
+    assert flow_0 < flow_1
+    # Test Flow methods
+    with pytest.raises(ValueError):
+        # Wrong available_bandwidth: negative number
+        flow_0.set_available_bandwidth(-100, 10)
+    with pytest.raises(ValueError):
+        # Wrong time_now: time should not be reversed
+        flow_0.set_available_bandwidth(100, -999)
+    assert flow_0.get_available_bandwidth() == \
+        tensor_unit.get_bytes_size()*8
+    assert flow_0.get_estimated_finish_time() == 10 + tensor_size
+    flow_0.set_available_bandwidth(tensor_unit.get_bytes_size() * 8 * 2, 11)
+    assert flow_0.get_estimated_finish_time() == 10 + 1 + (tensor_size - 1)/2
+    assert flow_0.get_available_bandwidth() == \
+        tensor_unit.get_bytes_size() * 8 * 2
+    flow_0.set_available_bandwidth(0, 14)
+    assert flow_0.get_estimated_finish_time() == float('inf')
+
+
+def create_test_flow(index, node_name, device_name, tensor_size, time_now):
+    # tensor_unit = Tensor('int32', 1)
+    # Initialize output_tensors
+    o_tensors = [Tensor('int32', 1) for _ in range(tensor_size)]
+    # Initialize node metadata
+    nodes_metadata = []
+    nodes_metadata.append(NodeMetadata(index=index, op="send", name=node_name,
+                                       device_name=device_name,
+                                       output_tensors=o_tensors,
+                                       execution_time=10, input_ids=[],
+                                       dependency_ids=[], successor_ids=[]))
+    device = FIFODevice(device_name)
+    node = Node(nodes_metadata[-1], device)
+    return Flow(node, time_now)
