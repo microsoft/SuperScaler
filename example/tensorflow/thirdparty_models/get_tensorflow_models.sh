646b6195dee7d6ceec6dc5438c1d4a80590f58ea
Guanbin Xu <xugb@mail.ustc.edu.cn>
2020-10-29 06:08:35 +0000

    Merged PR 1109: Examples: Add Feature - More models from thirdparty repo
    more models from 3rd-party repo;
    patch file:  only adjust the inceptionv4 for our framework.

---
+++ example/tensorflow/thirdparty_models/get_tensorflow_models.sh
@@ -0,0 +1,12 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+wget -c https://github.com/tensorflow/models/archive/v2.3.0.zip -O models.zip
+unzip -n models.zip
+rm models.zip
+patch -p0 < tf_models.patch
+python3 models-2.3.0/research/slim/setup.py install
+models_dir=`pwd`"/models-2.3.0/research/slim"
+wrapper_dir="$models_dir/nets/superscaler_wrapper.py"
+echo "Add $models_dir in your PYTHONPATH"
+echo "  use command: export PYTHONPATH=\$PYTHONPATH:$models_dir"
+echo "  see: $wrapper_dir for getting more models"
