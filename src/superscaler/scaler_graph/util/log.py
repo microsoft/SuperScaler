7341f0202bc2d892e5b879b28935b922942e5484
Guanbin Xu <xugb@mail.ustc.edu.cn>
2020-11-18 05:42:57 +0000

    Merged PR 1156: ScalerGraph: Add Feature - Enhance logger() in log.py
    add features:
    1. Save log file.
    2. check logger name in save() only for this PR

--- src/superscaler/scaler_graph/util/log.py
+++ src/superscaler/scaler_graph/util/log.py
@@ -1,9 +1,42 @@
 import logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] \
+import tempfile
+from enum import Enum, unique
+
+
+@unique
+class LOGGER_NAME(Enum):
+    SCALERGRAPH = "scalergraph"
+    DEFAULT = "scalergraph.default"
+
+
+_format = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] \
 - %(levelname)s: %(message)s')
 
 
-def logger(logger_name="default"):
+def save(filepath=None,
+         log_level=logging.DEBUG,
+         logger_name=LOGGER_NAME.SCALERGRAPH):
+    '''
+    Argus:
+        log_level: logging.DEBUG by default.
+        "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
+    '''
+    assert (logger_name in LOGGER_NAME)
+    logger = logging.getLogger(logger_name.value)
+    logger.setLevel(log_level)
+    if filepath is None:
+        _, filepath = tempfile.mkstemp(suffix='.log',
+                                       prefix='ScalerGraph',
+                                       text=True)
+    filehandler = logging.FileHandler(filename=filepath, mode='w')
+    filehandler.setFormatter(_format)
+    logger.addHandler(filehandler)
+    return filepath
+
+
+def logger(logger_name=LOGGER_NAME.DEFAULT):
     logger = logging.getLogger(logger_name)
+    handler = logging.StreamHandler()
+    handler.setFormatter(_format)
+    logger.addHandler(handler)
     return logger
