99ee24a57cef7fa44d3e55768a37b31dc4c9e913
Ze Gao <v-zegao@microsoft.com>
2020-12-07 07:44:58 +0000

    Merged PR 1192: Superscaler: Add Example - Add nnfusion example
    - add nnfusion mnist example
    - add nnfusion compile tool: tools/nnfusion_dp_single_host.sh

---
+++ tools/nnfusion_dp_single_host.sh
@@ -0,0 +1,79 @@
+# Copyright (c) Microsoft Corporation - All rights reserved
+# Licensed under the MIT License
+
+#!/bin/bash
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
+
+#TODO: test whether superscaler is installed
+#TODO: add cmdline arg parser
+#example
+#./nnfusion_dp_single_host_train.sh  data/mnist_mlp.onnx "-f onnx -p \"batch:3\" -fautodiff -ftraining_mode -fextern_result_memory=True" 10.0.0.25:2  data/resource_pool.yaml
+
+#user specified inputs
+MODEL_FILE_PATH=$(realpath $1)
+MODEL_COMPILING_OPTIONS="$2"
+#here, we only support one host mutilple worker
+DEPLOYMENT_CONFIG=$3
+CLUSTER_SPEC_FILE=$(realpath $4)
+
+
+NUM_PROCS_SAME_HOST=${DEPLOYMENT_CONFIG##*:}
+NNFUSION_EXE='./src/tools/nnfusion/nnfusion'
+DUMPED_GRAPH_FILENAME='graph'
+SUPERSCALER_COMPILING_OPTIONS="-fadd_sc_allreduce=true -fadd_sc_allreduce_fusion=true -fsc_allreduce_fusion_size=268435456 -fenable_export_graph=true -fnnfusion_graph_path=$DUMPED_GRAPH_FILENAME.pb"
+COMPILE_CMD="$NNFUSION_EXE $MODEL_FILE_PATH $MODEL_COMPILING_OPTIONS $SUPERSCALER_COMPILING_OPTIONS"
+
+echo "-- Creating build dir for compiling NNFusion model"
+rm -fr build && mkdir build && cd build && cp -r ../nnf_py . 
+pushd . > /dev/null
+echo "-- Cloning NNFusion repo"
+rm -fr NNFusion && git clone --quiet sysdnn@vs-ssh.visualstudio.com:v3/sysdnn/NNFusion/NNFusion && cd NNFusion
+echo "-- Building NNFusion"
+git checkout  v-zegao/superscaler-rc  --quiet && mkdir build && cd build && cmake .. > /dev/null
+make -j > /dev/null || make
+echo "-- Compiling model $MODEL_FILE_PATH"
+eval $COMPILE_CMD > /dev/null
+echo "-- Dumping NNFusion graph"
+python src/tools/serialize/nnfusion_serialize_tool.py $DUMPED_GRAPH_FILENAME.pb $DUMPED_GRAPH_FILENAME.pbtxt > /dev/null 2>&1
+popd > /dev/null
+
+#copy generated resources
+cp -r NNFusion/build/nnfusion_rt/cuda_codegen/* .
+cp -r NNFusion/build/para_info.json .
+cp -r NNFusion/build/$DUMPED_GRAPH_FILENAME.pbtxt .
+
+#modify cmakelist
+sed  -i 's/.*cuda_add_library.*/set(CMAKE_POSITION_INDEPENDENT_CODE ON)\ncuda_add_library(${TARGET_NAME} SHARED ${SRC})/' CMakeLists.txt
+
+echo "-- Generating superscaler runnig plan"
+python -c "from superscaler.nnfusion import generate_data_parallelism_plan; \
+           generate_data_parallelism_plan(\"$DUMPED_GRAPH_FILENAME.pbtxt\", \
+                                   ${NUM_PROCS_SAME_HOST}, \
+                                   \"${CLUSTER_SPEC_FILE}\", \
+                                   \"${PWD}\", \
+                                   communication_DSL='ring')" > /dev/null 
+
+# #cleanup
+rm -fr NNFusion
+
+MPIRUN=$(which mpirun)
+LAUNCH_CMD="$MPIRUN \
+--tag-output \
+--output-filename .result\
+"
+for i in $(seq 0 $(($NUM_PROCS_SAME_HOST-1)))
+do 
+    if [ "$i" == "0" ];then
+                LAUNCH_CMD="$LAUNCH_CMD -np 1 -host $DEPLOYMENT_CONFIG python nnf_py/train.py "$i/plan.json" "       
+        else
+                LAUNCH_CMD="$LAUNCH_CMD  : -np 1 -host $DEPLOYMENT_CONFIG python nnf_py/train.py "$i/plan.json" "
+    fi
+done
+
+echo "-- Writing launch command into launch.sh "
+echo $LAUNCH_CMD > launch.sh && chmod +x launch.sh
+echo "-- Compile successfully"
+
+
+
